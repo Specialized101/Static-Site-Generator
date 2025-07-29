54b8c2f187e883744c5ddc6b3479b4e021d8a50b
Specialized101 <yassinefares17@gmail.com>
2025-07-29 16:45:58 +0200

    Add ParentNode class

---
+++ src/parentnode.py
@@ -0,0 +1,19 @@
+
+from htmlnode import  HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("Parent nodes must have a tag")
+        if not self.children:
+            raise ValueError("Parent nodes must have children")
+
+        html = f"<{self.tag}{self.props_to_html()}>"
+        for child in self.children:
+            html += child.to_html()
+        html += f"</{self.tag}>" 
+        return html
+        
