d316fd8798d4f975b3770296fd9dc404d2cd0295
Specialized101 <yassinefares17@gmail.com>
2025-07-29 15:53:18 +0200

    Add Tests for LeafNode class

---
+++ src/test_leafnode.py
@@ -0,0 +1,23 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "boot.dev", {"href": "https://www.boot.dev"})
+        self.assertEqual(node.to_html(), '<a href="https://www.boot.dev">boot.dev</a>')
+    
+    def test_leaf_no_value(self):
+        with self.assertRaises(ValueError):
+            node = LeafNode("p", "", {"class": "para"})
+            node.to_html()
+
+    def test_leaf_no_tag(self):
+        node = LeafNode("", "boot.dev", {"href": "https://www.boot.dev"})
+        self.assertEqual(node.to_html(), "boot.dev")
+
+        
