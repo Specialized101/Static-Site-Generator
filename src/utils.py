6617498f24e321cd8ad5fbe131462a4f9976b411
Specialized101 <yassinefares17@gmail.com>
2025-07-30 16:03:24 +0200

    Add method to convert md string to textnodes

--- src/utils.py
+++ src/utils.py
@@ -84,3 +84,16 @@ def split_nodes_link(old_nodes):
         if current_text:
             new_nodes.append(TextNode(current_text, TextType.TEXT))
     return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+
+    return nodes
+
+
